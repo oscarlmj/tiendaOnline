e08312f4fa2526e519341752d34ad6ad4a7cf757
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-10-30 11:40:17 +0000

    Subida inicial del index

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BBDD Mi tienda Online</title>
+    <link rel="stylesheet" href="./CSS/index.css">
+</head>
+<body>
+    <header>
+        <li>
+            <ol>Mostrar lista de productos</ol>
+            <ol>Crear producto</ol>
+            <ol>Consultar listado</ol>
+            <ol>Modificar producto</ol>
+            <ol>Eliminar producto</ol>
+        </li>
+    </header>
+    <div id="opciones">
+    </div>
+</body>
+</html>
