525b86819db14cecaddb2ae876ce6eca5c4c84bf
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-10-30 12:14:41 +0000

    Subida inicial del crear_producto.php

---
+++ crear_producto.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BBDD Mi tienda Online</title>
+    <link rel="stylesheet" href="./CSS/index.css">
+</head>
+<body>
+    <header>
+        <ol>
+            <li>Mostrar lista de productos</li>
+            <li>Crear producto</li>
+            <li>Consultar listado</li>
+            <li>Modificar producto</li>
+            <li>Eliminar producto</li>
+        </ol>
+    </header>
+    <div id="opciones">
+    </div>
+</body>
+</html>
