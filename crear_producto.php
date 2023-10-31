c4577d9ba8c3abd2e16c394c4dd4de5c1adcf25a
Oscar Moreno <oscarlmorenojorge@gmail.com>
2023-10-31 12:23:15 +0000

    Estructura del crear_prodcuto.php finalizada

--- crear_producto.php
+++ crear_producto.php
@@ -1,5 +1,5 @@
 <?php
-
+require("C:/xampp/htdocs/tiendaOnline/connect.php");
 ?>
 
 <!DOCTYPE html>
@@ -14,13 +14,34 @@
     <header>
         <ol>
             <li>Mostrar lista de productos</li>
-            <li>Crear producto</li>
+            <li id="fijado">Crear producto</li>
             <li>Consultar listado</li>
             <li>Modificar producto</li>
             <li>Eliminar producto</li>
         </ol>
     </header>
     <div id="opciones">
+        <fieldset>
+            <legend>Datos del prodcuto</legend>
+            <form action="crear_producto.php">
+                <label for="">
+                    Nombre
+                    <input type="text" name="nombre" id="nombre">
+                </label>
+                <label for="">
+                    Precio
+                    <input type="text" name="precio" id="precio">
+                </label>
+                <label for="">
+                    Imagen
+                    <input type="file" name="imagen" id="imagen">
+                </label>
+                <label for="">
+                    Categoria
+                    <input type="text" name="categoria" id="categoria">
+                </label>
+            </form>
+        </fieldset>
     </div>
 </body>
 </html>
