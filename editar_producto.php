e414c4abd82daa7d2445c2564510dce065c0eefc
oscarlmj <oscarlmorenojorge@gmail.com>
2023-11-11 11:20:29 +0000

    Se añade el codigo para mostrar el desplegable si no se selecciona ningun producto en el index.php

--- editar_producto.php
+++ editar_producto.php
@@ -7,49 +7,67 @@ require("connect.php");
 $destino = "./img/";
 opendir($destino);
 //Recibe el id del producto seleccionado.
-$id = $_GET['id'];
 
-try {
-    //Consulta para las categorías.
-    $consulta = $conn->prepare("SELECT * FROM `categorías`");
-    $consulta->execute();
-    $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+//Ejecuta el codigo si el GET no esta vacio, es decir, haz seleccionado algun objeto en el index.php.
+if(!empty($_GET))
+{
+    //Almacena el id en una variable.
+    $id = $_GET['id'];
 
-    //Consulta para mostrar los datos del producto.
-    $consulta = $conn->prepare("SELECT * FROM `productos` WHERE id = $id");
-    $consulta->execute();
-    $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-    $campos = $resultado[0];
-} catch (PDOException $e) {
-    echo "Error al recuperar los datos: " . $e->getMessage();
-    die();
-}
+    try {
+        //Consulta para las categorías.
+        $consulta = $conn->prepare("SELECT * FROM `categorías`");
+        $consulta->execute();
+        $resultados = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    
+        //Consulta para mostrar los datos del producto.
+        $consulta = $conn->prepare("SELECT * FROM `productos` WHERE id = $id");
+        $consulta->execute();
+        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        $campos = $resultado[0];
 
-if (isset($_POST['nombre']) && isset($_POST['precio']) && !empty($_POST['categoria'])) {
-    $nombre = $_POST['nombre'];
-    $precio = $_POST['precio'];
-    $categoria = $_POST['categoria'];
+        
+    } catch (PDOException $e) {
+        echo "Error al recuperar los datos: " . $e->getMessage();
+        die();
+    }
+    
+    if (isset($_POST['nombre']) && isset($_POST['precio']) && !empty($_POST['categoria'])) {
+        //Almacena los valores del form en sus respectivas variables.
+        $nombre = $_POST['nombre'];
+        $precio = $_POST['precio'];
+        $categoria = $_POST['categoria'];
+    
+        //Realiza la validacion de los camppos.
+        $validacion = valida_nombre($nombre);
+    
+        //En caso de validacion correcta, ejecuta el programa.
+        if ($validacion) {
+            try {
+                //Comprueba si se inserto una imagen en el formulario y actualiza la imagen del producto.
+                if (isset($_FILES['imagen']['name']) && !empty($_FILES['imagen']['name'])) {
+                    $imagen_nombre = $_FILES['imagen']['name'];
+                    $nombre_temporal = $_FILES['imagen']['tmp_name'];
+                    $ruta_imagen = $destino . $imagen_nombre;
 
-    $validacion = valida_nombre($nombre);
+                    //Mueve la imagen al directorio.
+                    move_uploaded_file($nombre_temporal, $ruta_imagen);
 
-    if ($validacion) {
-        try {
-            //Comprueba si se inserto una imagen en el formulario y actualiza la imagen del producto.
-            if (isset($_FILES['imagen']['name']) && !empty($_FILES['imagen']['name'])) {
-                $imagen_nombre = $_FILES['imagen']['name'];
-                $nombre_temporal = $_FILES['imagen']['tmp_name'];
-                $ruta_imagen = $destino . $imagen_nombre;
-                move_uploaded_file($nombre_temporal, $ruta_imagen);
-                $sql = "UPDATE `productos` SET `Precio` = '$precio', `Categoría` = '$categoria', `Imagen` = '$ruta_imagen', `Nombre` = '$nombre' WHERE `productos`.`id` = '$id'";
-            } else {
-                // Si no se proporciona una nueva imagen, mantener la imagen actual en la base de datos
-                $sql = "UPDATE `productos` SET  `Nombre` = '$nombre', `Precio` = '$precio', `Categoría` = '$categoria' WHERE `productos`.`id` = '$id'";
+                    //Ejecuta la sentencia.
+                    $sql = "UPDATE `productos` SET `Precio` = '$precio', `Categoría` = '$categoria', `Imagen` = '$ruta_imagen', `Nombre` = '$nombre' WHERE `productos`.`id` = '$id'";
+                } else {
+                    // Si no se proporciona una nueva imagen, mantener la imagen actual en la base de datos
+                    $sql = "UPDATE `productos` SET  `Nombre` = '$nombre', `Precio` = '$precio', `Categoría` = '$categoria' WHERE `productos`.`id` = '$id'";
+                }
+
+                $conn->exec($sql);
+
+                //Redirige al usuario al index.php, para evitar que al refrescar la pagina inserte de nuevo los datos en la BBDD, ya que se almacenan en la caché.
+                header('Location: ./index.php');
             }
-
-            $conn->exec($sql);
-            header('Location: ./index.php');
-        } catch (PDOException $e) {
-            echo $sql . "<br>" . $e->getMessage();
+            catch (PDOException $e) {
+                echo $sql . "<br>" . $e->getMessage();
+            }
         }
     }
 }
@@ -74,7 +92,30 @@ if (isset($_POST['nombre']) && isset($_POST['precio']) && !empty($_POST['categor
         </ol>
     </header>
     <div id="opciones">
-        <form action="editar_producto.php?id=<?php echo $id; ?>" method="POST" enctype="multipart/form-data" name="form" autocomplete="off">            
+        <!-- Verificar si se proporciona un ID en la URL -->
+        <?php if (empty($_GET['id'])): ?>
+            <form action="editar_producto.php" method="GET">
+                <fieldset>
+                    <legend>Seleccionar Producto</legend>
+                    <label for="producto_select">
+                        Producto
+                        <select name="id" id="producto_select">
+                            <!--Ejecuta la sentancia para obtener el nombre de los productos-->
+                            <?php $consulta = $conn->prepare("SELECT * FROM productos");
+                                  $consulta->execute();
+                                  $productos = $consulta->fetchAll(PDO::FETCH_ASSOC);?>
+                            <!--Muestra el nombre de los productos en el desplegable-->
+                            <?php foreach ($productos as $producto): ?>
+                                <option value="<?php echo $producto['id']; ?>"><?php echo $producto['Nombre']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </label>
+                    <input type="submit" value="Seleccionar">
+                </fieldset>
+            </form>
+        <?php else: ?>
+            <!--Muestra el form con los datos del producto seleccionado en el index.php -->
+            <form action="editar_producto.php?id=<?php echo $id; ?>" method="POST" enctype="multipart/form-data" name="form" autocomplete="off">            
         <fieldset>
             <legend>Datos para modificar</legend>
                 <label for="">
@@ -103,6 +144,7 @@ if (isset($_POST['nombre']) && isset($_POST['precio']) && !empty($_POST['categor
                 <input type="submit" name="submit" value="Modificar">   
             </fieldset>
             </form>
+        <?php endif; ?>
     </div>
 </body>
 </html>
